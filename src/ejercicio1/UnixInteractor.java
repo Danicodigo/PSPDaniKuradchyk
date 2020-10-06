32cdf7205bee40185fb26a63a14fc25eec152e8d
XByDa <XByDa@DESKTOP-ASCHPDT>
2020-10-06 20:22:03 +0200

    Ejercicio3

--- src/ejercicio1/UnixInteractor.java
+++ src/ejercicio1/UnixInteractor.java
@@ -1,6 +1,7 @@
 package ejercicio1;
 
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,6 +12,7 @@ public class UnixInteractor {
 	public static void main(String[] command) {
 		// TODO Auto-generated method stub
 		String line;
+		
 		ProcessBuilder pb = new ProcessBuilder(command);
 		pb.redirectErrorStream(true);
 		try {
@@ -21,10 +23,8 @@ public class UnixInteractor {
 			System.out.println("La salida del proceso hijo " + Arrays.toString(command) + ":");
 			while ((line = br.readLine()) != null) {
 				System.out.println(line);
-				
 			}
 			is.close();
-			
 			
 		} catch (IOException e) {
 			System.out.println("Error ocurrió ejecutando el comando. Descripción: "+e.getMessage());
